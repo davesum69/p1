c34f42ba6cf0bc00da23064dd524d30765b947bc
davesum69 <dave_sum69@hotmail.com>
2025-11-22 18:50:44 +0530

    Second commit for 22nd nov 25

--- first.py
+++ first.py
@@ -12,11 +12,10 @@ print(x.find("bent"))
 
 #print("Hello "+y)
 
-lst = ["Alex","Ben","Chris","Donald","Ellis"]
+lst = ["Alex","Ben","Chris","Donald","Ellis","Freddy","Garry"]
 lst.append(23)
 lst.insert(3,"Donald")
 print(lst)
-
 print(lst.pop(2))
 
 print(lst)
