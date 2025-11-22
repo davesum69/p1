281fafd27186a30c590737422ff4f957e11eb31a
davesum69 <dave_sum69@hotmail.com>
2025-11-22 15:47:17 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,22 @@
+import pyjokes
+
+j = pyjokes.get_joke()
+
+print(j)
+
+x = "Hellbent"
+
+print(x.find("bent"))
+
+#y = input("Enter your name")
+
+#print("Hello "+y)
+
+lst = ["Alex","Ben","Chris"]
+lst.append(23)
+lst.insert(3,"Donald")
+print(lst)
+
+print(lst.pop(2))
+
+print(lst)
